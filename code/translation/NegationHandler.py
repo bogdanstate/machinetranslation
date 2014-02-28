3ea2d18ada462818cee33fc8f2fc02c1ec024d88
yoarcher <yuanyou1224@gmail.com>
2014-02-28 04:39:00 -0800

    www

--- code/translation/NegationHandler.py
+++ code/translation/NegationHandler.py
@@ -2,10 +2,13 @@
 
 class NegationHandler:
 	def reorderNeg(self,sentObj):
+		# sentObj is (words,lemmas,tags,orderString,feats)
 		# If sentObj words contain "inte":
 		#	if it follows a verb not equal to "vara":
 		#		exchange the position of "inte" and verb
 		# return 
+		
+
 
 	def translateNeg(self,sentObj):
 		# "not", "do not", "does not"
