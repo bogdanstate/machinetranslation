e67b18dccf0e8fc4f50d2e44dfc974ae53128ebc
yoarcher <yuanyou1224@gmail.com>
2014-02-27 20:21:03 -0800

    add prototype of negation handler

---
+++ code/translation/NegationHandler.py
@@ -0,0 +1,11 @@
+"""Rules for negation (eg. inte)"""
+
+class NegationHandler:
+	def reorderNeg(self,sentObj):
+		# If sentObj words contain "inte":
+		#	if it follows a verb not equal to "vara":
+		#		exchange the position of "inte" and verb
+		# return 
+
+	def translateNeg(self,sentObj):
+		# "not", "do not", "does not"
