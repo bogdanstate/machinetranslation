210bd0def02a54d318a16bf39c4800cc26794395
yoarcher <yuanyou1224@gmail.com>
2014-02-28 15:38:46 -0800

    not #

--- code/translation/SentenceSplitter.py
+++ code/translation/SentenceSplitter.py
@@ -40,12 +40,6 @@ class SentenceSplitter:
             #     break;
             line += 1
 
-        words.append("##")
-        lemmas.append("##")
-        tags.append("##")
-        feat.append("##")
-        order.append('%s')
-        
         group = (words,lemmas,tags,order,feat)
         split.append(rh.process(group))
         #split.append(group)
