be270dc22f7eca0e8ac856aff93c220e455bdc71
Bogdan State <bogdanstate@fb.com>
2014-02-28 01:40:59 -0800

    added bigrams pickle

--- code/translation/BigramOracle.py
+++ code/translation/BigramOracle.py
@@ -13,12 +13,12 @@ class BigramOracle:
   def show_me_the_path(self, candidates, num_desired):
     c = collections.Counter()
     for first, second in candidates:
-      first_stem = stemmer.stem(first)
-      second_stem = stemmer.stem(second)
+      first_stem = stemmer.stem(first.lower().split(' ')[-1])
+      second_stem = stemmer.stem(second.lower().split(' ')[0])
       c[(first, second)] = self.d[first_stem][second_stem]
-    return c.most_common(num_desired)
+    return [x for x in c.most_common(num_desired) if x[1] > 0]
 
 if __name__ == "__main__":
   o = BigramOracle("../../dictionaries/bigrams.pkl")
-  candidates = [("such","that"), ("green","witch"), ("your","mother"), ("European","Union")]
+  candidates = [("blerjrhka such","that fdkajfld"), ("green","witch"), ("your","mother"), ("European","Union"), ("as", "if")]
   print o.show_me_the_path(candidates, 3)
